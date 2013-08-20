bb8a06a340420adde462167159cfe6fc3a06f698
cheeseng <cheeseng@amaseng.com>
2013-08-20 18:16:54 +0800

    Initial version.

---
+++ app/controllers/Application.scala
@@ -0,0 +1,88 @@
+package controllers
+
+import play.api._
+import play.api.mvc._
+
+object Application extends Controller {
+
+  val latestScaladoc = "http://doc.scalatest.org/1.9.1"
+  val latestVersion = "1.9.1"
+  val milestoneVersion = "2.0.M5b"
+  val milestoneScaladoc = "http://www.artima.com/docs-scalatest-2.0.M5b"
+  val scaladocsLocation = "http://doc.scalatest.org"
+  val releasesLocation = "http://www.artima.com/downloadScalaTest"
+  val baseScalaVersion = "2.10.0"
+
+  def index = Action {
+    Ok(views.html.index())
+  }
+
+  def download = Action {
+    Ok(views.html.download())
+  }
+
+  def quickStart = Action {
+    Ok(views.html.quickStart())
+  }
+
+  def scaladoc = Action {
+    Ok(views.html.scaladoc())
+  }
+
+  def community = Action {
+    Ok(views.html.community())
+  }
+
+  def about = Action {
+    Ok(views.html.about())
+  }
+
+/*
+  def releaseNotes = Action {
+    Ok(views.html.releaseNotes())
+  }
+*/
+
+  def gettingStartedWithFunSuite = Action {
+    Ok(views.html.gettingStartedWithFunSuite())
+  }
+
+  def gettingStartedWithFunSpec = Action {
+    Ok(views.html.gettingStartedWithFunSpec())
+  }
+
+  def gettingStartedWithFeatureSpec = Action {
+    Ok(views.html.gettingStartedWithFeatureSpec())
+  }
+
+  def writingJunitAndTestngTestsInScala = Action {
+    Ok(views.html.writingJunitAndTestngTestsInScala())
+  }
+
+  def gettingStartedWithJunit3InScala = Action {
+    Ok(views.html.gettingStartedWithJunit3InScala())
+  }
+
+  def gettingStartedWithJunit4InScala = Action {
+    Ok(views.html.gettingStartedWithJunit4InScala())
+  }
+
+  def gettingStartedWithTestngInScala = Action {
+    Ok(views.html.gettingStartedWithTestngInScala())
+  }
+
+  // Redirect Scaladoc URLs
+  def scaladocs(release: String) = Action {
+    Redirect(scaladocsLocation + "/" + release + "/index.html")
+  }
+
+  def scaladocsFile(release: String, file: String) = Action {
+    Redirect(scaladocsLocation + "/" + release + "/" + file)
+  }
+
+  // Redirect legacy releases URLs (the are too big for free heroku,
+  // so I'll put them on Artima for now)
+  def releases(zipFile: String) = Action {
+    Redirect(releasesLocation + "/" + zipFile)
+  }
+}
---
+++ app/controllers/UserGuide.scala
@@ -0,0 +1,123 @@
+package controllers
+
+import play.api._
+import play.api.mvc._
+
+object UserGuide extends Controller {
+
+  def userGuideIndex = Action {
+    Ok(views.html.userGuide.userGuideIndex())
+  }
+
+  def usingAssertions = Action {
+    Ok(views.html.userGuide.usingAssertions())
+  }
+
+  def writingYourFirstTest = Action {
+    Ok(views.html.userGuide.writingYourFirstTest())
+  }
+
+  def taggingYourTests = Action {
+    Ok(views.html.userGuide.taggingYourTests())
+  }
+
+  def runningYourTests = Action {
+    Ok(views.html.userGuide.runningYourTests())
+  }
+
+  def sharingFixtures = Action {
+    Ok(views.html.userGuide.sharingFixtures())
+  }
+
+  def sharingTests = Action {
+    Ok(views.html.userGuide.sharingTests())
+  }
+
+  def usingMatchers = Action {
+    Ok(views.html.userGuide.usingMatchers())
+  }
+
+  def testingWithMockObjects = Action {
+    Ok(views.html.userGuide.testingWithMockObjects())
+  }
+
+  def testsAsSpecifications = Action {
+    Ok(views.html.userGuide.testsAsSpecifications())
+  }
+
+  def propertyBasedTesting = Action {
+    Ok(views.html.userGuide.propertyBasedTesting())
+  }
+
+  def otherGoodies = Action {
+    Ok(views.html.userGuide.otherGoodies())
+  }
+
+  def philosophyAndDesign = Action {
+    Ok(views.html.userGuide.philosophyAndDesign())
+  }
+
+  def designAndPhilosophy = Action {
+    Ok(views.html.userGuide.designAndPhilosophy())
+  }
+
+  def selectingAStyle = Action {
+    Ok(views.html.userGuide.selectingAStyle())
+  }
+
+  def usingJunitRunner = Action {
+    Ok(views.html.userGuide.usingJunitRunner())
+  }
+
+  def tableDrivenPropertyChecks = Action {
+    Ok(views.html.userGuide.tableDrivenPropertyChecks())
+  }
+
+  def generatorDrivenPropertyChecks = Action {
+    Ok(views.html.userGuide.generatorDrivenPropertyChecks())
+  }
+
+  def writingScalacheckStyleProperties = Action {
+    Ok(views.html.userGuide.writingScalacheckStyleProperties())
+  }
+
+  def usingTheRunner = Action {
+    Ok(views.html.userGuide.usingTheRunner())
+  }
+
+  def usingSelenium = Action {
+    Ok(views.html.userGuide.usingSelenium())
+  }
+
+  def invokingExecute = Action {
+    Ok(views.html.userGuide.invokingExecute())
+  }
+
+  def usingTheScalatestShell = Action {
+    Ok(views.html.userGuide.usingTheScalatestShell())
+  }
+
+  def usingScalatestWithSbt = Action {
+    Ok(views.html.userGuide.usingScalatestWithSbt())
+  }
+
+  def usingTheScalatestAntTask = Action {
+    Ok(views.html.userGuide.usingTheScalatestAntTask())
+  }
+
+  def matchersQuickReference = Action {
+    Ok(views.html.userGuide.matchersQuickReference())
+  }
+
+  def usingTheScalatestMavenPlugin = Action {
+    Ok(views.html.userGuide.usingTheScalatestMavenPlugin())
+  }
+
+  def customizingScalatest = Action {
+    Ok(views.html.userGuide.customizingScalatest())
+  }
+
+  def usingScalatestWithEclipse = Action {
+    Ok(views.html.userGuide.usingScalatestWithEclipse())
+  }
+}
