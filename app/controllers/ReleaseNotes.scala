bcea474e0f0df4bb2e6c13a57afd798ce549020f
cheeseng <cheeseng@amaseng.com>
2014-04-04 20:56:50 +0800

    Added release notes for 2.1.3, and make it to display at the top in landing page.

--- app/controllers/ReleaseNotes.scala
+++ app/controllers/ReleaseNotes.scala
@@ -24,6 +24,10 @@ object ReleaseNotes extends Controller {
     Ok(views.html.releaseNotes.releaseNotesIndex())
   }
 
+  def v213 = Action {
+    Ok(views.html.releaseNotes.v213())
+  }
+
   def v212 = Action {
     Ok(views.html.releaseNotes.v212())
   }
