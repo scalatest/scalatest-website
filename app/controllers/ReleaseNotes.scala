dd9496ffb5b25adf4441b09107386c014fc94306
cheeseng <cheeseng@amaseng.com>
2014-04-04 20:29:08 +0800

    Added release note for 2.1.2.

--- app/controllers/ReleaseNotes.scala
+++ app/controllers/ReleaseNotes.scala
@@ -24,6 +24,10 @@ object ReleaseNotes extends Controller {
     Ok(views.html.releaseNotes.releaseNotesIndex())
   }
 
+  def v212 = Action {
+    Ok(views.html.releaseNotes.v212())
+  }
+
   def v211 = Action {
     Ok(views.html.releaseNotes.v211())
   }
