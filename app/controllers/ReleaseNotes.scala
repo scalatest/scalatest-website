c166b742b57197045923a81223fc4ab823b9b936
bvenners <bill@artima.com>
2014-05-09 16:01:25 -0700

    Added rough sketch of 2.2.0 release notes.

--- app/controllers/ReleaseNotes.scala
+++ app/controllers/ReleaseNotes.scala
@@ -24,6 +24,10 @@ object ReleaseNotes extends Controller {
     Ok(views.html.releaseNotes.releaseNotesIndex())
   }
 
+  def v220 = Action {
+    Ok(views.html.releaseNotes.v220())
+  }
+
   def v215 = Action {
     Ok(views.html.releaseNotes.v215())
   }
