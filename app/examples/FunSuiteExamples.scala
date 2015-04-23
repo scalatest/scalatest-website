adaac1426027f2e57e31f1e658394ac6887d8e35
bvenners <bill@artima.com>
2015-04-23 07:07:22 -0700

    Fleshed out FunSuite examples.

--- app/examples/FunSuiteExamples.scala
+++ app/examples/FunSuiteExamples.scala
@@ -21,6 +21,7 @@ object FunSuiteExamples extends StyleTraitExamples {
 
   val description: String = """For teams coming from xUnit, FunSuite feels comfortable and familiar while still giving some of the benefits of BDD: FunSuite makes it easy to write descriptive test names, natural to write focused tests, and generates specification-like output that can facilitate communication among stakeholders."""
 
+/*
   val exampleUsage: String =
     """<span class="stImport">import org.scalatest.FunSuite</span>
       |<span class="stReserved">class</span> <span class="stType">SetSuite</span> <span class="stReserved">extends</span> <span class="stType">FunSuite</span> {
@@ -29,6 +30,64 @@ object FunSuiteExamples extends StyleTraitExamples {
       |    intercept[<span class="stType">NoSuchElementException</span>] { <span class="stType">Set</span>.empty.head }
       |  }
       |} """.stripMargin
+*/
+  val exampleUsage: String =
+    """<span class="stImport">import org.scalatest._</span>
+      |
+      |<span class="stReserved">class</span> <span class="stType">SetSpec</span> <span class="stReserved">extends</span> <span class="stType">FunSuite</span> {
+      |  <span class="stReserved">override</span> <span class="stReserved">def</span> withFixture(test: <span class="stType">NoArgTest</span>) = { <span class="stExplain">// Define a shared fixture</span>
+      |    <span class="stExplain">// Shared setup (run at beginning of each test)</span>
+      |    <span class="stReserved">try</span> test()
+      |    <span class="stReserved">finally</span> {
+      |      <span class="stExplain">// Shared cleanup (run at end of each test)</span>
+      |    }
+      |  }
+      |  <span class="stExplain">// Define tests with 'test', a test name string in parentheses,
+      |  // and test body in curly braces</span>
+      |  test(<span class="stLiteral">"An empty Set should have size 0"</span>) {
+      |    assert(<span class="stType">Set</span>.empty.size == <span class="stLiteral">0</span>)
+      |  }
+      |  <span class="stExplain">// To ignore a test, change 'test' to 'ignore'</span>
+      |  ignore(<span class="stLiteral">"Invoking head on an empty Set should produce NoSuchElementException"</span>) {
+      |    intercept[<span class="stType">NoSuchElementException</span>] {
+      |      <span class="stType">Set</span>.empty.head
+      |    }
+      |  }
+      |  <span class="stExplain">// Define a pending test by using (pending) for the body</span>
+      |  test(<span class="stLiteral">"An empty Set's isEmpty method should return false"</span>) (pending)
+      |  <span class="stExplain">// Tag a test by placing a tag object after the test name</span>
+      |  <span class="stImport">import tagobjects.Slow</span>
+      |  test(<span class="stLiteral">"An empty Set's nonEmpty method should return true"</span>, Slow) { 
+      |    assert(!<span class="stType">Set</span>.empty.nonEmpty)
+      |  }
+      |}
+      |
+      |<span class="stExplain">// Can also pass fixtures into tests with fixture.FunSuite</span>
+      |<span class="stReserved">class</span> <span class="stType">StringSpec</span> <span class="stReserved">extends</span> <span class="stType">fixture.FunSuite</span> {
+      |  <span class="stReserved">type</span> FixtureParam = <span class="stType">String</span> <span class="stExplain">// Define the type of the passed fixture object</span>
+      |  <span class="stReserved">override</span> <span class="stReserved">def</span> withFixture(test: <span class="stType">OneArgTest</span>) = {
+      |    <span class="stExplain">// Shared setup (run before each test), including...</span>
+      |    <span class="stReserved">val</span> fixture = <span class="stLiteral">"a fixture object"</span> <span class="stExplain">// ...creating a fixture object</span>
+      |    <span class="stReserved">try</span> test(fixture) <span class="stExplain">// Pass the fixture into the test</span>
+      |    <span class="stReserved">finally</span> {
+      |      <span class="stExplain">// Shared cleanup (run at end of each test)</span>
+      |    }
+      |  }
+      |  test(<span class="stLiteral">"The passed fixture can be used in the test"</span>) { s =&gt; <span class="stExplain">// Fixture passed in as s</span>
+      |    assert(s == <span class="stLiteral">"a fixture object"</span>)
+      |  }
+      |}
+      |
+      |@DoNotDiscover <span class="stExplain">// Disable discovery of a test class</span>
+      |<span class="stReserved">class</span> <span class="stType">InvisibleSpec</span> <span class="stReserved">extends</span> <span class="stType">FunSuite</span> { <span class="stBlockComment">/*code omitted*/</span> }
+      |
+      |@Ignore <span class="stExplain">// Ignore all tests in a test class</span>
+      |<span class="stReserved">class</span> <span class="stType">IgnoredSpec</span> <span class="stReserved">extends</span> <span class="stType">FunSuite</span> { <span class="stBlockComment">/*code omitted*/</span> }
+      |
+      |<span class="stImport">import tags.Slow</span>
+      |@Slow <span class="stExplain">// Mark all tests in a test class with a tag</span>
+      |<span class="stReserved">class</span> <span class="stType">SlowSpec</span> <span class="stReserved">extends</span> <span class="stType">FunSuite</span> { <span class="stBlockComment">/*code omitted*/</span> }
+      |""".stripMargin
 
   val play2Example: String =
     """<span class="stImport">import org.scalatest._</span>
