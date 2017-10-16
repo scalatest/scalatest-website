bb6a426a3297210c8e8dd81d613abc5c20d85e61
Peter <peter@Peters-MacBook-Pro.local>
2017-10-16 17:33:32 -0500

    modified to fix scalactic links per issue #1136; also fixed 'interpeter'-->'interpreter' typo

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -21,7 +21,7 @@ import examples.StyleTraitExamples
 
 object Application extends Controller {
 
-  val latestScaladoc = "http://doc.scalatest.org/3.0.1"
+  val latestScaladoc =          "http://doc.scalatest.org/3.0.1"
   val latestScalacticScaladoc = "http://doc.scalactic.org/3.0.1"
   val latestVersion = "3.0.4"
   val latestSuperSafeVersion = "1.1.2"				// updated per BV request -- 170130 PL
