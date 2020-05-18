90121fbb6f60248261824c23c97bdd9c4552041a
bvenners <bill@artima.com>
2020-05-18 15:17:05 -0700

    Accomodate newer Scaladoc style URLs starting with version 3.0.5.

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -111,8 +111,12 @@ class Application extends Controller {
     Redirect(scaladocsLocation + "/" + release + "/index.html")
   }
 
+  private val oldScaladocStyle30Releases = List("3.0.0", "3.0.1", "3.0.2", "3.0.3", "3.0.4")
   def scaladocsFile(release: String, file: String) = Action {
-    Redirect(scaladocsLocation + "/" + release + "/index.html#" + file)
+    if (release.startsWith("1.") || release.startsWith("2.") || oldScaladocStyle30Releases.contains(release))
+      Redirect(scaladocsLocation + "/" + release + "/index.html#" + file)
+    else
+      Redirect(scaladocsLocation + "/" + release + "/" + file.replaceAll("\\.", "/") + ".html")
   }
 
   // Redirect legacy releases URLs (the are too big for free heroku,
