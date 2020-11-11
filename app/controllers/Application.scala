51aba472405057f59ba0f1b79b63567292b1ffee
Chua Chee Seng <cheeseng@amaseng.com>
2020-11-11 16:49:53 +0800

    Changed latest supersafe version to 1.1.11.

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -142,7 +142,7 @@ object Application {
   val latestScaladoc =          "http://doc.scalatest.org/3.2.0"
   val latestScalacticScaladoc = "http://doc.scalactic.org/3.2.0"
   val latestVersion = "3.2.3"
-  val latestSuperSafeVersion = "1.1.10"
+  val latestSuperSafeVersion = "1.1.11"
   val milestoneVersion = "3.2.0"
   val milestoneJar = "https://oss.sonatype.org/content/groups/public/org/scalatest/scalatest_2.11/3.2.0-RC3/scalatest_2.11-3.2.0-RC3.jar"
   val latestJar = "https://oss.sonatype.org/content/groups/public/org/scalatest/scalatest-app_2.13/3.2.0/scalatest-app_2.13-3.2.0.jar"
