3d4e61c5373514ca93b5cbcd33bdd982ffa58035
Chua Chee Seng <cheeseng@amaseng.com>
2022-09-26 21:40:11 +0800

    Added scaladoc for 3.2.14.

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -139,7 +139,7 @@ class Application @Inject() (cc: ControllerComponents) extends AbstractControlle
 
 object Application {
 
-  val latestVersion = "3.2.13"
+  val latestVersion = "3.2.14"
   val latestSuperSafeVersion = "1.1.13"
   val milestoneVersion = "3.2.0"
   val milestoneJar = "https://oss.sonatype.org/content/groups/public/org/scalatest/scalatest_2.11/3.2.0-RC3/scalatest_2.11-3.2.0-RC3.jar"
