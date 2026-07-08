83650e99fa39d6db418260280ac50a637a6b6227
Chua Chee Seng <cheeseng@amaseng.com>
2026-07-08 21:26:30 +0800

    Improved release notes for 3.3.0.

--- app/controllers/ReleaseNotes.scala
+++ app/controllers/ReleaseNotes.scala
@@ -28,6 +28,11 @@ class ReleaseNotes @Inject() (cc: ControllerComponents) extends AbstractControll
   def v330 = Action {
     Ok(views.html.releaseNotes.v330())
   }
+
+  def v3220 = Action {
+    Ok(views.html.releaseNotes.v3220())
+  }
+
   def v3219 = Action {
     Ok(views.html.releaseNotes.v3219())
   }
